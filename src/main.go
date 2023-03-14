9baf02c3dbe95724993b4ecf30e7902462aa63ab
Aman Vivek Ramkumar <aman.ramkumar@nearform.com>
2023-03-14 16:55:57 +0000

    Remove redundant code

--- src/main.go
+++ src/main.go
@@ -4,19 +4,12 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"context"
-	"time"
-	"io"
 
 	"k8s-kurated-addons.cli/src/services/docker"
     "k8s-kurated-addons.cli/src/utils/logger"
 
 	"github.com/urfave/cli/v2"
-	"github.com/docker/docker/api/types"
 	"github.com/docker/docker/client"
-	"github.com/docker/docker/pkg/archive"
-	"github.com/docker/docker/pkg/jsonmessage"
-	"github.com/moby/term"
 )
 
 
@@ -62,51 +55,6 @@ func run(appName string, repoName string, dockerFilePath string) error {
 
     dockerService.Build(cli)
     dockerService.Push(cli)
-    return nil
-}
-
-func buildImage(dockerClient *client.Client, dockerFilePath string, appName string, repoName string) error {
-
-    ctx, cancel := context.WithTimeout(context.Background(), time.Second*120)
-    defer cancel()
-
-    buildContext, err := archive.TarWithOptions(dockerFilePath, &archive.TarOptions{})
-    if (err != nil) {
-        printErr("Failed to create build context", err)
-    }
-
-    buildOptions := types.ImageBuildOptions{
-        Dockerfile: "Dockerfile",
-        Tags:       []string{repoName + "/" + appName},
-        Remove:     true,
-    }
-
-    buildResponse, err := dockerClient.ImageBuild(ctx, buildContext, buildOptions)
-    if (err != nil) {
-        printErr("Failed to build docker image", err)
-    }
-
-    defer buildResponse.Body.Close()
-
-    displayMessagesStream(buildResponse.Body)
-
-    return nil
-}
-
-func displayMessagesStream(body io.Reader) error {
-
-    termFd, isTerm := term.GetFdInfo(os.Stdout)
-    err := jsonmessage.DisplayJSONMessagesStream(body, os.Stdout, termFd, isTerm, nil)
-    if (err != nil) {
-        printErr("Failed to display logs", err)
-    }
-
-    return nil
-}
-
-func printErr(message string, err error) error {
-    fmt.Println(message, err)
-    os.Exit(1)
 
     return nil
 }
