e04c99b1a6c16ec6c44d3cf11bb96a21b1f737a1
Luca Lanziani <luca@lanziani.com>
2023-05-07 17:24:11 +0200

    Add cli command to output dockerfiles

---
+++ src/cli/template.go
@@ -0,0 +1,26 @@
+package cli
+
+import (
+	"fmt"
+
+	"github.com/urfave/cli/v2"
+)
+
+func (c CLI) template(cCtx *cli.Context) error {
+	project := c.newProject(cCtx)
+	content, err := project.Dockerfile()
+	if err != nil {
+		return fmt.Errorf("Getting docker file %v", err)
+	}
+	fmt.Println(string(content))
+	return nil
+}
+
+func (c CLI) TemplateCMD() *cli.Command {
+	return &cli.Command{
+		Name:   "template",
+		Usage:  "output the docker file used for this project",
+		Flags:  Flags(Build),
+		Action: c.template,
+	}
+}
