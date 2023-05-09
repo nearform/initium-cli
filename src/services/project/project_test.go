0d2195864dc4b5925172e910bfdf3897dc722d91
Vinicius Gomes Marinho <viniciusmarinho@MacBook-Pro-de-Vinicius.local>
2023-05-08 21:34:41 -0300

    feature: adding more tests

--- src/services/project/project_test.go
+++ src/services/project/project_test.go
@@ -3,21 +3,23 @@ package project
 import (
 	"testing"
 	"fmt"
-
+	"os"
+	// _"embed"
 	"github.com/nearform/k8s-kurated-addons-cli/src/utils/defaults"
 )
 
+var projects = []map[string]string{
+	{"name": "node", "directory": "example"},
+	{"name": "go", "directory": "."},
+}
+
+var root = defaults.RootDirectoryTests
+
 func TestDetectType(t *testing.T) {
 
-	root := defaults.RootDirectoryTests
-	projs := []map[string]string{
-		{"name": "node", "directory": "example"},
-		{"name": "go", "directory": "."},
-	}
-
-	for _, value := range projs {
+	for _, value := range projects {
 		test_proj_type := Project{Name: value["name"], 
-							  Directory: fmt.Sprintf("%s%s", root, value["directory"])}
+							      Directory: fmt.Sprintf("%s%s", root, value["directory"])}
 
 		proj_type, err := test_proj_type.detectType()
 
@@ -28,7 +30,29 @@ func TestDetectType(t *testing.T) {
 		if proj_type != value["name"] {
 			t.Fatalf("Error: %s project not found", value["name"])
 		}
-	}
+	}	
+}
 
-	
+func TestTemplateFile(t *testing.T){
+
+	for _, value := range projects {
+		if _, err := os.Stat(fmt.Sprintf("%s/assets/docker/Dockerfile.%s.tmpl", root, value["name"])); err != nil {
+			t.Fatalf("Error: Dockerfile.%s.tmpl template not found", value["name"])
+		}
+	}
+}
+
+func TestLoadDockerfile(t *testing.T){
+	for _, value := range projects {
+		proj_dockerfile := Project{Name: value["name"], 
+							      Directory: fmt.Sprintf("%s%s", root, value["directory"]),
+								  Resources: defaults.TemplateFS}
+		_, err := proj_dockerfile.loadDockerfile()
+
+		if err != nil {
+			fmt.Println(err)
+			t.Fatalf(fmt.Sprintf("Error: %s", err))
+		}
+		
+	}
 }
--- src/utils/defaults/defaults.go
+++ src/utils/defaults/defaults.go
@@ -1,7 +1,14 @@
 package defaults
 
+import (
+	"embed"
+)
+
 const ProjectDirectory = "."
 const RepoName = "ghcr.io/nearform"
 const DockerfileName = "Dockerfile.kka"
 
 const RootDirectoryTests = "../../../"
+
+//go:embed assets
+var TemplateFS embed.FS
