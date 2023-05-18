deb9f297c6b1b3338094a34465e51dd7b47a53ea
Aman Vivek Ramkumar <aman.ramkumar@nearform.com>
2023-05-18 11:42:57 +0100

    Remove docker image default

--- src/utils/defaults/defaults.go
+++ src/utils/defaults/defaults.go
@@ -3,4 +3,3 @@ package defaults
 const ProjectDirectory  = "."
 const RepoName          = "ghcr.io/nearform"
 const DockerfileName    = "Dockerfile.kka"
-const DockerImage       = "gcr.io/knative-samples/helloworld-go"
