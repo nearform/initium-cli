37c9bbeffe99c0a261b79cd8d66fb09e52ac7ad2
Luca Lanziani <luca@lanziani.com>
2023-05-19 11:21:20 +0200

    Testing with datasource Docker

--- src/utils/defaults/defaults.go
+++ src/utils/defaults/defaults.go
@@ -3,7 +3,7 @@ package defaults
 const ProjectDirectory = "."
 const RepoName = "ghcr.io/nearform"
 const DockerfileName = "Dockerfile.kka"
-//  renovate: datasource=docker depName=node versioning=node
-const DefaultNodeRuntimeVersion="20"
+const DefaultNodeRuntimeVersion = "20"
+
 //  renovate: datasource=docker depName=golang
-const DefaultGoRuntimeVersion="1.19.8"
+const DefaultGoRuntimeVersion = "1.19.8"
