200eb57101f9b195f5ec1530656aed542ec709bc
Joshua <joshua@Joshuas-MacBook-Pro-2.local>
2023-05-19 12:30:15 +0200

    testing custom config

--- src/utils/defaults/defaults.go
+++ src/utils/defaults/defaults.go
@@ -5,7 +5,7 @@ const RepoName = "ghcr.io/nearform"
 const DockerfileName = "Dockerfile.kka"
 
 //  renovate: datasource=docker depName=node
-const DefaultNodeRuntimeVersion = "18"
+const DefaultNodeRuntimeVersion = "20"
 
 //  renovate: datasource=docker depName=golang
 const DefaultGoRuntimeVersion = "1.19.8"
