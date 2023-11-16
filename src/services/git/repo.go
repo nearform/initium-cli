cb0290f5be177b20451cb5d8d74c4ca2a6aa97f5
Marco Antonio Blanco <marcoantonio.blanco@nearform.com>
2023-11-16 12:18:36 +0100

    Fix hash

--- src/services/git/repo.go
+++ src/services/git/repo.go
@@ -112,6 +112,7 @@ func GetGithubOrg() (string, error) {
 
 func PublishCommentPRGithub (url string) {
 	fmt.Printf("Application URL: %s", url)
-	fmt.Printf("Commit hash: %s", GetHash())
+	commitSha, err := git.GetHash()
+	fmt.Printf("Commit hash: %s", commitSha)
 	fmt.Printf("Timestamp: %v", time.Now())
 }
