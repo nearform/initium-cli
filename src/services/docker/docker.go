90aa6d8210fdb78514062659275a407b2abbf8f3
Marco Antonio Blanco <mablanco@users.noreply.github.com>
2023-11-27 17:46:53 +0100

    feat: enforce linux/amd64 images (#154)

--- src/services/docker/docker.go
+++ src/services/docker/docker.go
@@ -139,6 +139,7 @@ func (ds DockerService) Build() error {
 		Dockerfile: ds.DockerFileName,
 		Tags:       []string{ds.dockerImage.LocalTag()},
 		Remove:     true,
+		Platform:	"linux/amd64",
 	}
 
 	// Build the image
